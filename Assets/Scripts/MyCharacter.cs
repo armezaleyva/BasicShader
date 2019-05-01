b64b22d0777b6d032d29eab308c54eb11d1867b5
Armando Meza Leyva <armezaleyva@gmail.com>
2019-05-01 16:27:56 -0700

    Initial commit

---
+++ Assets/Scripts/MyCharacter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MyCharacter : MonoBehaviour
+{
+    Renderer render;
+    [SerializeField]
+    float colorPower = 1f;
+    [SerializeField]
+    Slider sldColorPower;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        render = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        render.material.SetFloat("_ColorPower", sldColorPower.value);
+    }
+}
