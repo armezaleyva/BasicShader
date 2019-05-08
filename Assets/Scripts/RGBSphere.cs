e95d5dc10154dfea66576cdc5eb1c38bc9d8e877
Armando Meza Leyva <armezaleyva@gmail.com>
2019-05-08 08:04:15 -0700

    Slider RGB

---
+++ Assets/Scripts/RGBSphere.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RGBSphere : MonoBehaviour
+{
+    Renderer render;
+    [SerializeField]
+    Slider sldR;
+    [SerializeField]
+    Slider sldG;
+    [SerializeField]
+    Slider sldB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        render = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        render.material.SetFloat("_Albedo_R", sldR.value);
+        render.material.SetFloat("_Albedo_G", sldG.value);
+        render.material.SetFloat("_Albedo_B", sldB.value);
+    }
+}
